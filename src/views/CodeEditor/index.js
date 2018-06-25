5f4d5f6c23b4df80a437adfb18bd1ed5184f0ed4
Bukhari Muhammad <bukharim96@gmail.com>
2018-06-25 23:36:09 +0200

    Create sample CodeEditor preview

--- src/views/CodeEditor/index.js
+++ src/views/CodeEditor/index.js
@@ -3,21 +3,55 @@ import {
   StyleSheet,
   Text,
   View,
-  Image
+  ScrollView
 } from 'react-native'
 
-import commonStyles from '../themes/commonStyles'
-
-import vsnLogoSrc from '../../_resources/logos/SplashScreen.scale-200_dark.png'
+import TabHeaders from './TabHeaders'
 
 export default (props) => {
   return (
     <View style={styles.container}>
-      <Text style={commonStyles.h1}>
-        Welcome to Visual Studio Native!
+      <TabHeaders />
+      
+      {/* CODE EDITOR PREVIEW */}
+      <Text
+        selectable={true}
+        // selectionColor="blue" // Android // @todo bridge to Windows
+        style={styles.codeEditor}
+      >
+        <Text style={[styles.codeEditor, {color: "#f92672"}]} children="import" />
+        {" "}
+        {"React"}
+        {" "}
+        <Text style={[styles.codeEditor, {color: "#f92672"}]} children="from" />
+        {" "}
+        <Text style={[styles.codeEditor, {color: "#e6db74"}]} children="'react'" />
+        {"\n"}
+        <Text style={[styles.codeEditor, {color: "#f92672"}]} children="import" />
+        {" "}
+        {"{"}
+        {"\n"}
+        {"  "}
+        {"StyleSheet,"}
+        {"\n"}
+        {"  "}
+        {"Text,"}
+        {"\n"}
+        {"  "}
+        {"View,"}
+        {"\n"}
+        {"  "}
+        {"Image"}
+        {"\n"}
+        {"}"}
+        {" "}
+        <Text style={[styles.codeEditor, {color: "#f92672"}]} children="from" />
+        {" "}
+        <Text style={[styles.codeEditor, {color: "#e6db74"}]} children="'react-native'" />
+        {"\n"}
+        
+        <View style={{backgroundColor: "#ddd", width: 2, height: 22}} />
       </Text>
-      <Image source={vsnLogoSrc} />
-      <Text style={styles.instructions}>To get started, edit App.windows.js</Text>
     </View>
   )
 }
@@ -26,12 +60,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 5,
     //justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     backgroundColor: '#1e1e1e'
   },
+  codeEditor: {
+    color: '#fff',
+    fontWeight: "normal",
+    fontSize: 17,
+    lineHeight: 22,
+    letterSpacing: 0,
+    padding: 15
+  },
   instructions: {
-    textAlign: 'center',
-    color: '#777',
+    textAlign: "center",
+    color: "#777",
     marginTop: 15
   }
 })
