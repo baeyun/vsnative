85b04d609e66af385d8127281d6effcda7542657
bukharim96 <bukharim96@gmail.com>
2018-07-16 08:52:20 +0200

    More Menubar changes

---
+++ src/components/windows/Menubar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import {
+  requireNativeComponent,
+  ViewPropTypes,
+  View
+} from 'react-native';
+
+var iface = {
+  name: 'MenuButton',
+  propTypes: {
+    data: PropTypes.array,
+    ...ViewPropTypes // include the default view properties
+  },
+};
+
+const Menubar = requireNativeComponent('VSNativeMenubar', iface);
+
+export default ({style, ...rest}) => {
+  return <View
+    style={style}
+    children={
+      <Menubar {...rest} style={{flex: 1}} />
+    } />
+}
