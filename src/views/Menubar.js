17469776b0b6258e4cfb4447967e851558a12b40
bukharim96 <bukharim96@gmail.com>
2018-07-16 08:50:33 +0200

    Native Menubar: First working version

--- src/views/Menubar.js
+++ src/views/Menubar.js
@@ -1,16 +1,47 @@
-import React from 'react'
-import { View } from 'react-native'
+// import React from 'react'
+// import { View } from 'react-native'
 
-import MenuButton from '../components/windows/MenuButton'
+// import MenuButton from '../components/windows/MenuButton'
 
-export default ({display}) => {
-  return (
-    <View style={{flex: .23, flexDirection: 'row', backgroundColor: '#ddd'}}>
-      <MenuButton content="File" style={{width: 35, height: 25}} />
-      <MenuButton content="Edit" style={{width: 35, height: 25}} />
-      <MenuButton content="Selection" style={{width: 63, height: 25}} />
-      <MenuButton content="View" style={{width: 42, height: 25}} />
-      <MenuButton content="Help" style={{width: 42, height: 25}} />
-    </View>
-  )
-}
+// // const fileMenuData = [
+// //   {name: "New File", callback: ""},
+// //   {name: "New Window", callback: ""},
+// //   {seperator},
+// //   {name: "Open File", callback: ""},
+// //   {name: "Open Folder", callback: ""},
+// //   {seperator},
+// //   {name: "Open Workspace", callback: ""},
+// //   {name: "Open recent", submenu: [
+// //     {name: "./views/CodeEditor/index.js", callback: ""},
+// //     {name: "../components/windows/MenuButton", callback: ""},
+// //     {name: "./guppy/scripts/", callback: ""},
+// //     {name: "../static/img/logo.png", callback: ""},
+// //     {name: "../vsnative/", callback: ""}
+// //   ]},
+// //   {seperator},
+// //   {name: "Save", callback: ""},
+// //   {name: "Save As", callback: ""},
+// //   {name: "Save All", callback: ""},
+// //   {name: "Save", callback: ""},
+// //   {seperator},
+// //   {name: "Auto Save", toggle: true, callback: ""},
+// //   {seperator},
+// //   {name: "Preferences", callback: ""},
+// //   {seperator},
+// //   {name: "Revert File", callback: ""},
+// //   {name: "Close Editor", callback: ""},
+// //   {name: "Close Folder", callback: ""},
+// //   {name: "Close Window", callback: ""}
+// // ]
+
+// export default ({display}) => {
+//   return (
+//     <View style={{flex: .23, flexDirection: 'row', backgroundColor: '#ddd'}}>
+//       <MenuButton content="File" style={{width: 35, height: 25}} />
+//       <MenuButton content="Edit" style={{width: 35, height: 25}} />
+//       <MenuButton content="Selection" style={{width: 64, height: 25}} />
+//       <MenuButton content="View" style={{width: 42, height: 25}} />
+//       <MenuButton content="Help" style={{width: 42, height: 25}} />
+//     </View>
+//   )
+// }
