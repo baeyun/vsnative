df940fc5c39f1eba4fa957b2c6a44e8cb45dd34e
bukharim96 <bukharim96@gmail.com>
2018-07-17 21:15:25 +0200

    Removed unnecessary files

--- src/components/IconOcticons.js
+++
@@ -1,5 +0,0 @@
-import { createIconSetFromIcoMoon } from 'react-native-vector-icons'
-
-export default createIconSetFromIcoMoon(
-  require('../static/icons/octicons-selection.json') // config file
-)
--- src/components/IconSeti.js
+++
@@ -1,5 +0,0 @@
-import { createIconSetFromIcoMoon } from 'react-native-vector-icons'
-
-export default createIconSetFromIcoMoon(
-  require('../static/icons/seti-selection.json') // config file
-)
